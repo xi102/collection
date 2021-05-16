713a0abe1c19d41571bf80684f84d9d7c435c637
zhang.j.k <zhangjk67@gmail.com>
2021-05-16 13:18:51 +0800

    move router to routers dir

--- main.go
+++ main.go
@@ -1,45 +1,6 @@
 package main
 
-import (
-	"fmt"
-	"net/http"
-	"path/filepath"
-
-	"github.com/gin-gonic/gin"
-)
-
 func main() {
 	router := InitRouter()
 	router.Run(":8080")
-}
-
-func InitRouter() *gin.Engine {
-	router := gin.Default()
-	// Set a lower memory limit for multipart forms (default is 32 MiB)
-	router.MaxMultipartMemory = 8 << 20 // 8 MiB
-	router.Static("/index", "./public")
-	router.StaticFS("/files", http.Dir("./upload"))
-	router.POST("/upload/", func(c *gin.Context) {
-		name := c.PostForm("name")
-		email := c.PostForm("email")
-
-		// Multipart form
-		form, err := c.MultipartForm()
-		if err != nil {
-			c.String(http.StatusBadRequest, fmt.Sprintf("get form err: %s", err.Error()))
-			return
-		}
-		files := form.File["files"]
-
-		for _, file := range files {
-			filename := filepath.Base(file.Filename)
-			if err := c.SaveUploadedFile(file, filepath.Join("upload", filename)); err != nil {
-				c.String(http.StatusBadRequest, fmt.Sprintf("upload file err: %s", err.Error()))
-				return
-			}
-		}
-
-		c.String(http.StatusOK, fmt.Sprintf("Uploaded successfully %d files with fields name=%s and email=%s.", len(files), name, email))
-	})
-	return router
 }
