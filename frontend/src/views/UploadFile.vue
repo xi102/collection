fc5ab117df5cd757568e5628f2415282d7a94110
Beethoven <yifu.liu@std.uestc.edu.cn>
2021-05-18 16:03:08 +0800

    edit frontend

--- frontend/src/views/UploadFile.vue
+++ frontend/src/views/UploadFile.vue
@@ -1,15 +1,19 @@
 <template>
-  <div style="margin:20px;">
-    <v-file-input 
-      show-size 
-      counter 
-      chips 
-      multiple 
-      label="点击此处选择文件" 
-      ref="myfile" 
-      v-model="files"
-    />
-    <v-btn text @click="submitFiles"><h3>提交上传</h3></v-btn>
+  <div>
+    <h1 style="padding-top:40px;padding-bottom:20px;">师兄师姐们把文件放到这里来</h1>
+    <div style="margin:20px;">
+        <v-file-input 
+        filled
+        show-size 
+        counter 
+        chips 
+        multiple 
+        label="点击此处选择文件" 
+        ref="myfile" 
+        v-model="files"
+        />
+        <v-btn text @click="submitFiles"><h3>提交上传</h3></v-btn>
+    </div>
   </div>
 </template>
 
--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,4 +1,12 @@
 module.exports = {
+  devServer: {
+    public:'0.0.0.0',
+    port:3000,
+    overlay: {
+      warnings: true,
+      errors: true
+    },
+  },
   transpileDependencies: [
     'vuetify'
   ]
