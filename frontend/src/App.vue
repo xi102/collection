8dd81ef9694c057f09b6abb0b9b0ba34f3cd1e3c
zhang.j.k <zhangjk67@gmail.com>
2021-05-18 18:16:27 +0800

    add login

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -5,6 +5,7 @@
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link> |
       <router-link to="/testfiles">Upload Files</router-link>
+      <router-link to="/login">Login</router-link>
     </div>
   </div>
 </template>
