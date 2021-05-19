56ce2b32ea76039ef53183f0f5f42c260d69e47c
zhang.j.k <zhangjk67@gmail.com>
2021-05-19 19:12:18 +0800

    add alert button

--- frontend/src/views/UploadFile.vue
+++ frontend/src/views/UploadFile.vue
@@ -16,6 +16,44 @@
             <v-btn text @click="submitFiles" class="mt-10"><h3>提交上传</h3></v-btn>
         </div>
     </v-container>
+    <v-dialog
+        v-model="dialog"
+        width="500"
+    >
+        <template v-slot:activator="{ on, attrs }">
+        <v-btn
+            color="red lighten-2"
+            dark
+            v-bind="attrs"
+            v-on="on"
+        >
+            Click Me
+        </v-btn>
+        </template>
+
+        <v-card>
+        <v-card-title class="headline grey lighten-2">
+            提示
+        </v-card-title>
+
+        <v-card-text>
+            恭喜你，上传成功了
+        </v-card-text>
+
+        <v-divider></v-divider>
+
+        <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn
+            color="primary"
+            text
+            @click="dialog = false"
+            >
+            好的
+            </v-btn>
+        </v-card-actions>
+        </v-card>
+    </v-dialog>
   </div>
 </template>
 
